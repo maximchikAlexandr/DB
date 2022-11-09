1b3b3bed9fa9a5493ad9d839f855194988d46e44
maximchikAlexandr <maximchik.alexandr@yandex.ru>
2022-11-09 22:25:23 +0300

    add object con and cur

--- db_psycopg2.py
+++ db_psycopg2.py
@@ -8,3 +8,12 @@ load_dotenv()
 password_db  = os.getenv('password_db')
 
 conn = psycopg2.connect(database="postgres", user="postgres", password=password_db)
+
+cur = conn.cursor()
+
+cur.execute('SELECT 1')
+data = cur.fetchone()
+print(data, type(data))
+
+cur.close()
+conn.close()
